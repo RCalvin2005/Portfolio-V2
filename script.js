c4f37eeaad79816dc9446c77200b364f71111f84
Richard Calvin Yong <r.calvin.yong@gmail.com>
2022-11-30 22:38:12 +0700

    updated home display

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ window.onload = function() {
     // Add padding to ensure content not blocked by nav
     var navHeight = document.querySelector("nav").clientHeight
     var main = document.querySelector("main")
-    main.style.paddingTop = String(navHeight + 10) + "px";
+    main.style.paddingTop = String(navHeight) + "px";
 
+    // Ensure each section fills viewport
+    var vh = window.innerHeight
+    var sections = document.querySelectorAll("section")
+    sections.forEach(function(section) {
+        section.style.minHeight = String(vh - navHeight) + "px";
+    })
 }
 
 // https://github.com/twbs/bootstrap/issues/1768
