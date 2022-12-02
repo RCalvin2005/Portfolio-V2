a6da62b661f3374081c3fec192cceeffaa590c5a
Richard Calvin Yong <r.calvin.yong@gmail.com>
2022-12-03 00:02:45 +0700

    created projects section foundation

--- script.js
+++ script.js
@@ -11,8 +11,23 @@ window.onload = function() {
     var vh = window.innerHeight
     var container = document.querySelectorAll(".section-container")
     container.forEach(function(container) {
-        container.style.minHeight = String(vh - navHeight) + "px";
+        if (container.id == "home_container")
+        {
+            container.style.minHeight = String(vh - navHeight) + "px";
+        }
+        else
+        {
+            container.style.minHeight = String(vh - 2 * navHeight) + "px";
+        }
     })
+
+    // Ensures carousel items fill viewport
+    var projectsHeading = document.querySelector("#projects_heading");
+    var style = getComputedStyle(projectsHeading);
+    var headingHeight = projectsHeading.offsetHeight + parseInt(style.marginTop) + parseInt(style.marginBottom) 
+    var carouselItem = document.querySelector(".carousel-item");
+    carouselItem.style.height = String(vh - navHeight - headingHeight) + "px";
+
 }
 
 // https://github.com/twbs/bootstrap/issues/1768
