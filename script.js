896ded34a1e674950c30269925aed783e9d22d89
Richard Calvin Yong <r.calvin.yong@gmail.com>
2022-12-03 23:08:58 +0700

    added all project previews

--- script.js
+++ script.js
@@ -21,13 +21,6 @@ window.onload = function() {
         }
     })
 
-    // Ensures carousel items fill viewport
-    var projectsHeading = document.querySelector("#projects_heading");
-    var style = getComputedStyle(projectsHeading);
-    var headingHeight = projectsHeading.offsetHeight + parseInt(style.marginTop) + parseInt(style.marginBottom) 
-    var carouselItem = document.querySelector(".carousel-item");
-    carouselItem.style.height = String(vh - navHeight - headingHeight) + "px";
-
 }
 
 // https://github.com/twbs/bootstrap/issues/1768
