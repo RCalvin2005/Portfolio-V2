62802c35aa7b78901d46bad83af383e14d86d9f3
Richard Calvin Yong <r.calvin.yong@gmail.com>
2022-12-08 12:05:01 +0700

    adjusted reveal delay

--- reveal.js
+++ reveal.js
@@ -10,7 +10,7 @@ const sr = ScrollReveal ({
 document.addEventListener("loaded", function() {
 
     // Home
-    sr.reveal("#home_heading", {});
+    sr.reveal("#home_heading", {})
     sr.reveal("#home_subheading", {delay: 200});
     sr.reveal("#home_text", {delay: 400});
     sr.reveal(".home_pic", {delay: 600});
@@ -18,33 +18,28 @@ document.addEventListener("loaded", function() {
     sr.reveal(".fa-square", {delay: 900});
 
     // About
-    sr.reveal("#about_heading", {});
-    sr.reveal("#about_text", {delay: 200});
+    sr.reveal("#about_text", {});
     sr.reveal("#about_pic", {delay: 400});
 
     // Qualifications
-    sr.reveal("#qualifications_heading", {});
-    sr.reveal(".qualification_org", {delay: 200});
-    sr.reveal(".qualification_icon", {delay: 400});
-    sr.reveal(".qualification_desc", {delay: 600});
-    sr.reveal(".progress", {delay: 800});
-    sr.reveal(".progress-bar", {delay: 850});
+    sr.reveal(".qualification_org", {});
+    sr.reveal(".qualification_icon", {delay: 200});
+    sr.reveal(".qualification_desc", {delay: 400});
+    sr.reveal(".progress", {delay: 600});
+    sr.reveal(".progress-bar", {delay: 650});
 
     // Skills
-    sr.reveal("#skills_heading", {});
-    sr.reveal(".skill_icon", {delay: 200, interval: 200});
-    sr.reveal(".skill_name", {delay: 300, interval: 200});
-    sr.reveal(".fa-star", {delay: 400, interval: 50});
-    sr.reveal("#skills_credit", {delay: 1400});
+    sr.reveal(".skill_icon", {interval: 200});
+    sr.reveal(".skill_name", {delay: 100, interval: 200});
+    sr.reveal(".fa-star", {delay: 200, interval: 50});
+    sr.reveal("#skills_credit", {delay: 1200});
 
     // Projects
-    sr.reveal("#projects_heading", {});
-    sr.reveal("#projects_carousel", {delay: 300});
+    sr.reveal("#projects_carousel", {delay: 200});
 
     // Contact
-    sr.reveal("#contact_heading", {});
-    sr.reveal(".contact_icon", {delay: 200, interval: 200});
-    sr.reveal(".contact_info", {delay: 350, interval: 200});
+    sr.reveal(".contact_icon", {interval: 200});
+    sr.reveal(".contact_info", {delay: 150, interval: 200});
 })
 
 
